30a8992b06283a79573a5f6a4ef3c83aa3caa79c
namedias <brian99na@gmail.com>
2022-07-05 01:57:10 -0700

    added spring animations

--- src/components/model.js
+++ src/components/model.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGLTF } from "@react-three/drei";
+import { useSpring, a } from '@react-spring/three'
 
 function Model() {
+    const [spread, setSpread] = useState(false)
+
     const keycaps = useGLTF("keycaps.gltf", true);
     const top = useGLTF("top.gltf", true);
     const bottom = useGLTF("bottom.gltf", true);
@@ -28,21 +31,33 @@ function Model() {
       }
     })
 
+    const handleSpread = () => {
+      setSpread(!spread)
+    } 
+
+    const spreadAnimation = useSpring({
+      capPosition: spread ? [0, 3, -2] : [0, 0, 0],
+      topPosition: spread ? [0, 2.25, -1] : [0, 0, 0],
+      platePosition: spread ? [0, 1.5, 0] : [0, 0, 0],
+      botPosition: spread ? [0, 0.75, 2] : [0, 0, 0],
+      plateRotation: spread ? [0.2, 0, 0] : [0, 0, 0]
+    })
+
     return(
-      <>
+      <group onClick={handleSpread}>
         <mesh castShadow>
-          <primitive object={keycaps.scene} dispose={null} />
+          <a.primitive rotation={spreadAnimation.plateRotation} position={spreadAnimation.capPosition} object={keycaps.scene} dispose={null} />
         </mesh>
         <mesh>
-          <primitive object={top.scene} dispose={null} />
+          <a.primitive position={spreadAnimation.topPosition} object={top.scene} dispose={null} />
         </mesh>
         <mesh>
-          <primitive object={bottom.scene} dispose={null} />
+          <a.primitive position={spreadAnimation.botPosition} object={bottom.scene} dispose={null} />
         </mesh>
         <mesh>
-          <primitive object={plate.scene} dispose={null} />
+          <a.primitive position={spreadAnimation.platePosition} object={plate.scene} dispose={null} />
         </mesh>
-      </>
+      </group>
     )
   }
   
